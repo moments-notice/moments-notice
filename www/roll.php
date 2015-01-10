113ea83aa9e28964f94aa254b2ac9dac1d07d9c2
Micah Walter <micah@micahwalter.com>
2015-01-10 18:26:32 +0000

    adding roll pages

---
+++ www/roll.php
@@ -0,0 +1,13 @@
+<?php
+
+	include("include/init.php");
+
+	loadlib("rolls");
+
+	$id = get_int64("id");
+
+	$roll = rolls_get_by_id($id);
+
+	$smarty->assign('roll', $roll);
+	$smarty->display('page_roll.txt');
+	exit();
