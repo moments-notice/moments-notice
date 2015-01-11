c4db8f94f30b6e8f2d1a2ade78db2ad381982785
Micah Walter <micahwalter@users.noreply.github.com>
2015-01-11 11:49:54 -0500

    stop live coding in github now

--- www/rolls.php
+++ www/rolls.php
@@ -5,6 +5,11 @@
 	loadlib("rolls");
 
 	$rolls = rolls_get_all();
+	
+	foreach( $rolls['rows'] as $roll){
+		$rsp = rolls_get_inflated_roll_by_id($roll['id']);
+		dumper($rsp);
+	}
 
 	$smarty->assign('rolls', $rolls['rows']);
 	$smarty->display('page_rolls.txt');
